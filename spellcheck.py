2c10258000f33a8baf159a8f38802868bac52a94
DJ Jagannathan <desjag@gmail.com>
2014-04-09 00:39:14 -0400

    removed duplicates + replaced vowels // need to calculate Lev

--- spellcheck.py
+++ spellcheck.py
@@ -1,6 +1,6 @@
 with open("/usr/share/dict/words") as dictionary:
     library = dictionary.read().lower().splitlines()
-print library, len(library)
+print library[100143], len(library)
 
 """
 keeps /n
@@ -9,11 +9,46 @@ dictionary = open("/usr/share/dict/words")
 library = dictionary.readlines()
 """
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
-vowels = 'aeiou'
-
+vowels = ('aeiou')
 word = input("> ").lower()
 print word
+'''
+#set up trie data structure
+class trieStruc:
+	def __init__(self):
+		self.word = None
+		self.children = {}
 
+		def insert(self,word):
+			element = self
+			for letter in word:
+				if letter not in self.children:
+					self.children[letter] = trieStruc()
+
+				element = self.children[letter]
+
+			element.word = word
+
+#fill trie with words from dictionary			
+trie = trieStruc()
+for one in library:
+	trie.insert(one)						
+
+print trie
+'''
+
+#checks for duplicates and removes them and replaces all vowels with 'a'
+def duplicates(w, v):
+	import itertools
+	import re
+	#duplicates
+	noDup = ''.join(ch for ch, _ in itertools.groupby(w))
+	#vowels
+	allNew = re.sub("[aeiou]", "a", noDup)
+	return allNew
+
+
+#binary search for words without any duplicates and vowels replaced with 'a'
 def binarySearch():
     global found, lower, upper, midpoint
     lower = 0
@@ -24,18 +59,21 @@ def binarySearch():
         print "M: ", midpoint
         print "U: ", upper
         print "L: ", lower
-        if library[midpoint] < word:
+        if library[midpoint] < newWord:
             lower = midpoint+1
-        elif library[midpoint] > word:
+        elif library[midpoint] > newWord:
             upper = midpoint-1
         else:
             found = True
 
     if found:
-        print "The word is spelled correctly", midpoint
+        print "Found it at", midpoint
     else:
-    	correct(word)
+		print "NO SUGGESTION"
 
-"""def correct(word):"""
+#gets new word with no duplicates/replaced vowels
+newWord = duplicates(word, "aeiou")
 
+#searches for new word in dictionary + calculates Lev + provides suggestion
 binarySearch()
+print newWord
